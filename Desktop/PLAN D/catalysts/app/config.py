43f9ac646ef6f72fde83a6c99c5fda2e20ab7a8e
C P SREERAG <144200108+Cp-sreerag@users.noreply.github.com>
2026-01-08 19:15:07 +0530

    Delete Desktop directory

--- Desktop/PLAN D/catalysts/app/config.py
+++
@@ -1,8 +0,0 @@
-# ===================== app/config.py ==========================
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-MODEL_PATH = BASE_DIR / ".." / "models" / "stock_models.keras"  # adjust if different
-CACHE_PATH = BASE_DIR / ".." / "cache"
-CACHE_PATH.mkdir(exist_ok=True)
-DEFAULT_LOOKBACK = 60
